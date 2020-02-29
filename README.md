b27c0e696473157daa65fa30f1d477f5748a9dfb
Kimberly Cable <cableredi@comcast.net>
2020-02-28 18:47:17 -0700

    readme updates

--- README.md
+++ README.md
@@ -1,26 +1,50 @@
-# Express Boilerplate!
+# MyClassroom ![MyClassroom](./src/images/Logo-thumbnail.svg)
 
-This is a boilerplate project used for starting new projects!
+My Classroom is a simple and effective way for teachers/professors/tutors/etc to effectively communicate to his/her students the upcoming assignments, tests, quizzes, projects, etc.
 
-## Set up
+My Classroom is easy to use
 
-Complete the following steps to start a new project (NEW-PROJECT-NAME):
+**Teachers:** Just add your assignments and your students will be able to see what's up next
 
-1. Clone this repository to your local machine `git clone BOILERPLATE-URL NEW-PROJECTS-NAME`
-2. `cd` into the cloned repository
-3. Make a fresh start of the git history for this project with `rm -rf .git && git init`
-4. Install the node dependencies `npm install`
-5. Move the example Environment file to `.env` that will be ignored by git and read by the express server `mv example.env .env`
-6. Edit the contents of the `package.json` to use NEW-PROJECT-NAME instead of `"name": "express-boilerplate",`
+**Students:** Just log in and view the Calendar to see what is coming up
 
-## Scripts
+**Parents:** Just log in as your student and you can see what your child's assignments are to help keep them on track
 
-Start the application `npm start`
+## Check it out
+[https://myclassroom.now.sh/](https://myclassroom.now.sh/)
 
-Start nodemon for the application `npm run dev`
 
-Run the tests `npm test`
+## Screenshots
+#### Landing page: [https://myclassroom.now.sh/](https://myclassroom.now.sh/)
+![Landing Page](./src/mages/Landing.png)
 
-## Deploying
+#### Calendar page:
+![Calendar Page](./src/images/Calendar.png)
 
-When your new project is ready for deployment, add a new Heroku application with `heroku create`. This will make a new git remote called "heroku" and you can then `npm run deploy` which will push to this remote's master branch.
+#### List of Assignments per Day page:
+![List of Assignments per Day Page](./src/images/Calendar-Assignments.png)
+
+#### Classes page:
+![Classes Page](./src/images/Classes.png)
+
+
+## MyClassroom API
+**/api/assignments**
+CRUD operations for assignments table
+
+**/api/classes/:**
+CRUD operations for classes table
+
+**/api/users/:**
+Operations for users table
+
+**/api/auth/:**
+Validate Authorization
+
+## Source Code
+MyClassroom Client: [https://github.com/cableredi/MyClassroom](https://github.com/cableredi/MyClassroom)
+
+MyClassroom API: [https://github.com/cableredi/myclassroom-api](https://github.com/cableredi/myclassroom-api)
+
+## Technology Used
+HTML5, SCSS/CSS, Node.js, React, Express, PostgreSQL
--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 const fixtures = require('./myclassroom-fixtures');
 
 describe('Auth Endpoints', () => {
-  let db
+  let db;
 
   const testUsers = fixtures.makeUsersArray();
   const testUser = testUsers[0];
@@ -15,21 +15,63 @@ describe('Auth Endpoints', () => {
       connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
-    db.raw('TRUNCATE users, classes, assignments RESTART IDENTITY CASCADE')
+    db.raw('TRUNCATE users RESTART IDENTITY CASCADE')
   });
 
-  afterEach('cleanup', () => db.raw('TRUNCATE users, classes, assignments RESTART IDENTITY CASCADE'));
+  afterEach('cleanup', () => db.raw('TRUNCATE users RESTART IDENTITY CASCADE'));
 
   after('disconnect from db', () => db.destroy())
 
   describe(`POST /api/auth/login`, () => {
-    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
-      const userValidCreds = {
+    beforeEach('insert users', () => {
+      return db
+        .into('users')
+        .insert(testUsers)
+    })
+
+    const requiredFields = ['user_name', 'password']
+
+    requiredFields.forEach(field => {
+      const loginAttemptBody = {
         user_name: testUser.user_name,
         password: testUser.password,
       }
+
+      it(`responds with 400 required error when '${field}' is missing`, () => {
+        delete loginAttemptBody[field]
+
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          })
+      })
+    })
+
+    it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidUser = { user_name: 'user-not', password: 'existy' }
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidUser)
+        .expect(400, { error: `Incorrect user_name or password` })
+    })
+
+    it(`responds 400 'invalid user_name or password' when bad password`, () => {
+      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidPass)
+        .expect(400, { error: `Incorrect user_name or password` })
+    })
+
+    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+      const userValidCreds = {
+        user_name: testUser.user_name,
+        password: 'password',
+      }
       const expectedToken = jwt.sign(
-        { user_id: testUser.user_id },
+        { user_id: testUser.user_id, role: testUser.role },
         process.env.JWT_SECRET,
         {
           subject: testUser.user_name,
@@ -40,26 +82,6 @@ describe('Auth Endpoints', () => {
       return supertest(app)
         .post('/api/auth/login')
         .send(userValidCreds)
-        .expect(200, {
-          authToken: expectedToken,
-        })
-    })
-  })
-
-  describe(`POST /api/auth/refresh`, () => {
-    it(`responds 200 and JWT auth token using secret`, () => {
-      const expectedToken = jwt.sign(
-        { user_id: testUser.user_id },
-        process.env.JWT_SECRET,
-        {
-          subject: testUser.user_name,
-          expiresIn: process.env.JWT_EXPIRY,
-          algorithm: 'HS256',
-        }
-      )
-      return supertest(app)
-        .post('/api/auth/refresh')
-        .set('Authorization', fixtures.makeAuthHeader(testUser))
         .expect(200, {
           authToken: expectedToken,
         })
